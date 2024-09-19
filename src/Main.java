e069780f358eb88e567687a63552ce75ca857d9c
46948549y <46948549y@iespoblenou.org>
2024-09-19 19:53:42 +0200

    Added changes

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,17 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-
 public class Main {
     public static void main(String[] args) {
-        String inputPath = "";
-        String outputPath = "";
-
-        String text = readFile(inputPath);
-
-
-    }
-
-    private static String readFile(String path) {
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                line = br.readLine();
-            }
-            return sb.toString();
-        } catch (Exception e) {
-            System.out.println(e + " Error al intentar leer el archivo de entrada.");
-            return null;
+        // Si no se han introducido argumentos
+        if (args.length < 1) {
+            System.out.println("No se han introducido opciones");
+        } 
+        // Si se introduce text
+        else if (args[0].toString().toLowerCase().equals("text")){
+            TextInput.execute();
         }
-    }
+        // Si se introduce binary 
+        else if (args[0].toString().toLowerCase().equals("binary")) {
 
-    private static String writeFile(String path) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-
-        } catch (Exception e) {
-            System.out.println(e + " Error al intentar escribir en el archivo de salida.");
         }
-    }
+        System.exit(0);
+    }    
 }
